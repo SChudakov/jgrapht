2ba48ab87b4cbe3644ade1fdf488f2a3a283d98c
Semen Chudakov <semen.chudakov7@gmail.com>
2020-07-07 23:21:43 +0300

    test prototype

---
+++ jgrapht-core/src/test/java/org/jgrapht/alg/similarity/ZhangShashaTreeEditDistanceTest.java
@@ -0,0 +1,38 @@
+package org.jgrapht.alg.similarity;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ZhangShashaTreeEditDistanceTest {
+
+    @Test
+    public void testTED_BothTreesAreEmpty() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testTED_FirstTreeIsEmpty() {
+
+    }
+
+    @Test
+    public void testTED_SecondTreeIsEmpty() {
+
+    }
+
+    @Test
+    public void testTED_TreesOfEqualSize() {
+
+    }
+
+    @Test
+    public void testTED_FirstTreeLarger() {
+
+    }
+
+    @Test
+    public void testTED_SecondTreeLarger() {
+
+    }
+}
